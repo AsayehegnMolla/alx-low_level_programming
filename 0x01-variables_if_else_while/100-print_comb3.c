c1276f244c1a90ecf2e1f148852b1553f4e38d3c
AsayehegnMolla <ajuwajuwa55@gmail.com>
2022-04-08 18:23:44 -0700

    Combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = '0'; num1 <= '9'; num1++)
+	{
+		num2 = num1 + 1;
+		for (num2 = '0'; num2 <= '9'; num2++)
+		{
+			if (num1 == num2)
+				continue;
+			else
+			{
+				putchar(num1);
+				putchar(num2);
+			}
+		}
+		if (num1 < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	return (0);
+}
