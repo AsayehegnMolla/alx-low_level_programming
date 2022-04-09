705e5cb16face252ade8382a1bdadc616e05642f
AsayehegnMolla <ajuwajuwa55@gmail.com>
2022-04-08 17:17:24 -0700

    Combo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,8 +14,11 @@ int main(void)
 	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
-		putchar(',');
-		putchar(' ');
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 
 	return (0);
