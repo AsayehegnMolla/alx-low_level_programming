2c66090090ab173b6e3ce6b7b7573b7b9653ca33
AsayehegnMolla <ajuwajuwa55@gmail.com>
2022-04-12 13:40:59 -0700

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,8 @@
 #include "main.h"
 
 /*
- * print_alphabet_x10 - print lower alpabhets 10 times
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase
+ *  
  */
 
 void print_alphabet_x10(void)
