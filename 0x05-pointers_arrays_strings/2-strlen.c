918b5efd92855f67af92e6de2dfd5d0526648a50
birukwubishet <birukwubishet4560@gmail.com>
2022-04-18 14:54:42 -0700

    update commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,16 @@
+#include "main.h"
 
+/**
+ * _strlen - return the length of a a string
+ * @s: string to be checked
+ *
+ * Return: lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(*s);
+	return (len);
+}
