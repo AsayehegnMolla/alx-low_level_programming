0f4c0af1871aac992140c11927a3e39a857b3001
AsayehegnMolla <96838241+AsayehegnMolla@users.noreply.github.com>
2022-04-18 14:48:02 -0700

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,18 @@
 #include "main.h"
 
+/**
+ * reset_to_98 - update a variable to 98 using a pointer
+ * @n: points to the variable n
+ *
+ * Return: void funtion
+ */
 
 void swap_int(int *a, int *b)
 {
 	int x;
 
-	x = a;
-	a = b;
-	b = x;
+	x = *a;
+	*a = *b;
+	*b = x;
 }
 
