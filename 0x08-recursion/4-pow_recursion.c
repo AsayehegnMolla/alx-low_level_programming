c2ba212dd518572d44fb058de220e3fd92c8264e
birukwubishet <birukwubishet4560@gmail.com>
2022-04-28 15:00:45 -0700

    n(th) power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - calculate the n(th)power of a number
+ * @x: number to be used
+ * @y: the exponent
+ *
+ * Return: the n(th) power of a number
+
+int _pow_recursion(int x, int y)
+{
+	int product;
+
+	if ( y < 0)
+		return (-1);
+	else if (y == 0)
+		return (0);
+	product = _pow_recursion(x, y - 1);
+
+	return (x*x);
+}
