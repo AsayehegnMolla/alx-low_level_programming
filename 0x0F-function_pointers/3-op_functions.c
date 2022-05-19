3ff0f4e3f0a1ef3e7688cbb1c0c532de9ffe3ce7
birukwubishet <birukwubishet4560@gmail.com>
2022-05-19 04:25:26 -0700

    commit

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,19 @@
+#include "3-clac.h"
+
+/**
+ * op_add - function that adds a and b
+ * @a: first integer
+ * @b: second integer
+ *
+ * Returns: a + b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b)
+}
+
+/**
+ * op_sub - function that substracts a and b
+ * @a: first integer
+ * @b: second integer
